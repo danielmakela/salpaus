f7244e45c3755ce6212deb64bfa6d62c4322842f
danielmakela <88773240+danielmakela@users.noreply.github.com>
2021-08-19 07:31:14 +0000

    2021.8.18

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("täällä ollaan!");
+
+let ika=15
+ika = ika + 1;
+let ikakirjaimina = "15";
+kakirjaimina = ikakirjaimina +1;
+console.log(ikakirjaimina)
+console.log("sinun ikasi on: + ika");
+
+let juuvaiei =true;
+console.log("boolean:" + juuvaiei);
+let nimi = "Daniel Jim Makela";
+console.log("kirjoita isolla: " + nimi.toUpperCase());
+console.log("splittausharjoitus: " + nimi.split(" ")[0]);
+
+for(let i=0; i<=10; i++){
+  console.log("loopin iteraatioluku on: " + i);
+}
+
+if(ika => 18){
+  console.log("kasva vielä vähän...");
+}
+else{
+  console.error("kasva viellä vähän...");
+}
+function laskutehtava(ekaArvo, tokaArvo){
+return ekaArvoo + tokaArvo;
+}
+console.log(ikakirjaimina)
+
+ika = ika + 1; 
+ikakirjaimina = ikakirjaimina + 1;
+
+console.log(ikakirjaimina);
+
+console.log ("sinun ikäsi on: " + ika);
+console.log("boolean: " + juuvaiei);
+
+let pvm = "2021/08/18";
+console.log (pvm.split("/")[0]);
+function pvmFin(vuosi,kk){
+return kk+-.-+vuosi;
+)
+console.log(pvmFin(pvm.split("/")[0], pvm.split("/")[1()));
